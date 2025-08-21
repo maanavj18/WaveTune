696101ac899b9d2e36461708ff8eae2cfddb1b58
maanavjajodia18 <maanavjajodia@gmail.com>
2025-08-21 04:37:00 -0400

    Added trial song

--- gesture_control/tracking.py
+++
@@ -1,6 +0,0 @@
-import cv2
-import mediapipe as mp
-
-class HandTracker:
-    def __init__(self, max_num_hands=2, min_detection_confidence=0.7, min_tracking_confidence=0.5):
-        self.hands = mp.solutions.hands.Hands()
